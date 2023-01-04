447b27ef7f0c7f044d04a1b39cfec25d9391c3ed
JoelMathewC <joelmathewcherian@gmail.com>
2023-01-04 20:02:38 +0530

    Date formatting in query cards fixed

--- lib/view/loading/loading.dart
+++ lib/view/loading/loading.dart
@@ -1,6 +1,8 @@
+import 'package:clijeo_public/view/core/theme/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
   static String id = "Loading";
@@ -8,8 +10,13 @@ class Loading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(color: Colors.red),
+    return const Scaffold(
+      backgroundColor: AppTheme.backgroundColor,
+      body: Center(
+          child: SpinKitRing(
+        color: AppTheme.primaryColor,
+        lineWidth: 5,
+      )),
     );
   }
 }
