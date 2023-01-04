d73e590232a8b667e67447e70675ab7223102274
JoelMathewC <joelmathewcherian@gmail.com>
2023-01-04 19:50:50 +0530

    splash screen added

--- lib/view/splash_screen/splash_screen.dart
+++ lib/view/splash_screen/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:clijeo_public/view/core/theme/app_color.dart';
+import 'package:clijeo_public/view/core/theme/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -8,8 +10,16 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeConfig = SizeConfig(context);
     return Scaffold(
-      body: Container(color: Colors.blue),
-    );
+        backgroundColor: AppTheme.primaryColor,
+        body: Center(
+          child: SizedBox(
+            width: sizeConfig.safeBlockSizeHorizontal(0.7),
+            child: Image.asset(
+              "assets/clijeo.jpeg",
+            ),
+          ),
+        ));
   }
 }
